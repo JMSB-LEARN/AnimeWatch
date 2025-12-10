16c168fb82fe88f6d0000e9116ff84ae253f2e8f
JMSB-LEARN <jmarquess04@iesalbarregas.es>
2025-12-10 16:58:48 +0000

    Anglar 20

---
+++ src/app/pages/my-animes-page/my-animes-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-animes-page',
+  imports: [],
+  templateUrl: './my-animes-page.html',
+  styleUrl: './my-animes-page.css',
+})
+export class MyAnimesPage {
+
+}
